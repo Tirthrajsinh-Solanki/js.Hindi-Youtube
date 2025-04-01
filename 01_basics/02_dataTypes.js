fe9ef4d9ec39bf3889c585261b2f8627b0ad6420
Tirthrajsinh-Solanki <niketasolanki2805@gmail.com>
2025-04-01 17:04:29 +0000

    commit config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert('hello') // we are using nodejs,not browser
+
+// code readability should be high
+console.log(3+3);
+
+console.log("Tirth")
+
+let name = "Tirth"
+let age = 18
+let islggedIn = false
+
+// number => 2 to power 53
+// bigint
+// sting
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol 
+
+
+//object
+
+console.log(typeof"Tirth")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
